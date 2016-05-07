4a7d07347cc1bf06f8b3cf5bd6d648a7912ef6be
Daniela Mazza <danielamazza@gmail.com>
2016-05-07 14:33:59 +0200

    First commit

--- Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
+++ Assignments/Lesson01-Assignment01-Case-Statement/student-start/module2_lesson1_formative.rb
@@ -1,15 +1,69 @@
 some_var = "false"
-another_var = "nil"
+another_var = nil
 
-if some_var == "pink elephant"
-  puts "Don't think about the pink elephant!"
-
-elsif another_var.nil?
-  puts "Question mark in the method name?"
-
-elsif some_var == false
-  puts "Looks like this one should execute"
-
-else
-  puts "I guess nothing matched... But why?"
+case some_var
+when "pink elephant"
+	puts "Don't think about the pink elephant!"
+when false
+	case another_var
+	when nil
+		puts "Question mark in the method name?"
+	end
+	
+			
 end
+
+# if some_var == "pink elephant"
+#   puts "Don't think about the pink elephant!"
+
+# elsif another_var.nil?
+#   puts "Question mark in the method name?"
+
+# elsif some_var == false
+#   puts "Looks like this one should execute"
+
+# else
+#   puts "I guess nothing matched... But why?"
+# end
+
+
+
+
+
+
+
+
+
+
+# case some_var 
+# when "pink elephant"
+#  	puts "Don't think about the pink elephant!"
+# when "false"
+# 	puts "Question mark in the method name?"
+# else
+# 	case another_var
+# 	when nil
+#   		puts "Question mark in the method name?"
+#   	else puts "I guess nothing matched... but why?"
+#   	end
+# end
+
+# case false
+# 	when some_var
+# 		puts "Don't think about the pink elephant!"
+# 	when another_var
+# 		puts "Question mark in the method name?"
+# 	else
+# 		case some_var
+# 		when "pink elephant" 
+# 			puts "Don't think about the pink elephant"
+# 		else
+# 			puts "I guess nothing matched... but why?"
+# 		end
+# 	end
+# end
+
+
+
+
+
